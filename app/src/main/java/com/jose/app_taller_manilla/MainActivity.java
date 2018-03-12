327b73eb9039b551f9b6762842a0a478fd181ded
jchavez5 <josechavez1796@gmail.com>
2018-03-12 23:13:48 +0100

    modificacion de los testing, testeo terminado metodo provado correctamente

--- app/src/main/java/com/jose/app_taller_manilla/MainActivity.java
+++ app/src/main/java/com/jose/app_taller_manilla/MainActivity.java
@@ -16,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView resultado;
     private Resources recursos;
 
-    private Spinner materiales_spinner,dijes_spinner,tipos_spinner,tipospago_spinner;
-    private String materiales[],dijes[],tipos[],tipospago[];
+    private Spinner materiales_spinner, dijes_spinner, tipos_spinner, tipospago_spinner;
+    private String materiales[], dijes[], tipos[], tipospago[];
     private Button boton;
 
     @Override
@@ -25,59 +25,73 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cantidad=findViewById(R.id.id_cantidad);
-        resultado=findViewById(R.id.id_total);
-        recursos=this.getResources();
+        cantidad = findViewById(R.id.id_cantidad);
+        resultado = findViewById(R.id.id_total);
+        recursos = this.getResources();
 
-        materiales_spinner=findViewById(R.id.id_material);
-        dijes_spinner=findViewById(R.id.id_dije);
-        tipos_spinner=findViewById(R.id.id_tipo);
-        tipospago_spinner=findViewById(R.id.id_tipopago);
+        materiales_spinner = findViewById(R.id.id_material);
+        dijes_spinner = findViewById(R.id.id_dije);
+        tipos_spinner = findViewById(R.id.id_tipo);
+        tipospago_spinner = findViewById(R.id.id_tipopago);
 
 /*crear las lista en los spinner*/
-        materiales=recursos.getStringArray(R.array.Lista_materiales);
-        ArrayAdapter<String> adapter= new ArrayAdapter(this,android.R.layout.simple_spinner_item,materiales);
+        materiales = recursos.getStringArray(R.array.Lista_materiales);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, materiales);
         materiales_spinner.setAdapter(adapter);
 
-        dijes=recursos.getStringArray(R.array.Lista_dije);
-        ArrayAdapter<String> adapter1 =new ArrayAdapter(this,android.R.layout.simple_spinner_item,dijes);
+        dijes = recursos.getStringArray(R.array.Lista_dije);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, dijes);
         dijes_spinner.setAdapter(adapter1);
 
-        tipos=recursos.getStringArray(R.array.Lista_tipo);
-        ArrayAdapter<String> adapter2 =new ArrayAdapter(this,android.R.layout.simple_spinner_item,tipos);
+        tipos = recursos.getStringArray(R.array.Lista_tipo);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, tipos);
         tipos_spinner.setAdapter(adapter2);
 
-        tipospago=recursos.getStringArray(R.array.Lista_moneda);
-        ArrayAdapter<String> adapter3 =new ArrayAdapter(this,android.R.layout.simple_spinner_item,tipospago);
+        tipospago = recursos.getStringArray(R.array.Lista_moneda);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, tipospago);
         tipospago_spinner.setAdapter(adapter3);
 
-        boton=findViewById(R.id.id_boton);
+        boton = findViewById(R.id.id_boton);
 
     }
-    public void calculo(View v){
-        double  cant ;/*variables de uso*/
-        int opciones_material,opciones_dije,opciones_tipo,opciones_moneda;
+
+    public void calculo(View v) {
+        double cant;/*variables de uso*/
+        int opciones_material, opciones_dije, opciones_tipo, opciones_moneda;
         resultado.setText("");/*setear la caja*/
 
-        cant=Double.parseDouble(cantidad.getText().toString());/*obtiene la cantidad*/
+        cant = Double.parseDouble(cantidad.getText().toString());/*obtiene la cantidad*/
 
-        opciones_material  = materiales_spinner.getSelectedItemPosition();/*el spinner material en su posicion*/
-        opciones_dije=dijes_spinner.getSelectedItemPosition();/*el spinner dije en su posicion*/
-        opciones_tipo=tipos_spinner.getSelectedItemPosition();/*el spinner tipo en su posicion*/
-        opciones_moneda=tipospago_spinner.getSelectedItemPosition();/*el spinner moneda en su posicion*/
-
-       double   operacion=Metodos.calculo_Spinner(cant,opciones_material,opciones_dije,opciones_tipo,opciones_moneda);
-       resultado.setText(""+String.format("%.2f",operacion));
+        opciones_material = materiales_spinner.getSelectedItemPosition();/*el spinner material en su posicion*/
+        opciones_dije = dijes_spinner.getSelectedItemPosition();/*el spinner dije en su posicion*/
+        opciones_tipo = tipos_spinner.getSelectedItemPosition();/*el spinner tipo en su posicion*/
+        opciones_moneda = tipospago_spinner.getSelectedItemPosition();/*el spinner moneda en su posicion*/
+        if (validar()){
+            double operacion = Metodos.calculo_Spinner(cant, opciones_material, opciones_dije, opciones_tipo, opciones_moneda);
+            resultado.setText("" + String.format("%.2f", operacion));
         }
-
-
+    }
 
 
     public boolean validar(){
-        if (cantidad.getText().toString().isEmpty()) {
-        cantidad.requestFocus();
-        cantidad.setError(recursos.getString(R.string.cantidad_valor));
-        return false;
+        if (cantidad.getText().toString().equals("")){
+            cantidad.requestFocus();
+            cantidad.setError(recursos.getString(R.string.cantidad_valor));
+            return false;
+        }
+        int numeroMenoQueCero=Integer.parseInt(cantidad.getText().toString());
+        if (numeroMenoQueCero<0){
+            cantidad.requestFocus();
+            cantidad.setError(recursos.getString(R.string.error_mayor));
+            return false;
+
+        }
+        return true;
     }
-    return true;}
 }
+
+
+
+
+
+
--- app/src/test/java/com/jose/app_taller_manilla/ExampleUnitTest.java
+++ app/src/test/java/com/jose/app_taller_manilla/ExampleUnitTest.java
@@ -10,14 +10,33 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-
-
-
-
-
-
-
-
-
+    @Test
+    public void cuero_martillo_oro_dolares(){
+        double  cant = 5;
+        int opcion_material=0,opcion_dije=0,opcion_tipo=0,opcion_tipomoneda=0;
+        double resultado = Metodos.calculo_Spinner(cant,opcion_material,opcion_dije,opcion_tipo,opcion_tipomoneda);
+        assertEquals(500,resultado,0 );
+    }
+    @Test
+    public void cuero_martillo_oro_pesos(){
+        double  cant = 5;
+        int opcion_material=0,opcion_dije=0,opcion_tipo=0,opcion_tipomoneda=1;
+        double resultado = Metodos.calculo_Spinner(cant,opcion_material,opcion_dije,opcion_tipo,opcion_tipomoneda);
+        assertEquals(1600000,resultado,0 );
+    }
+    @Test
+    public void cuerda_ancla_niquel_dolares(){
+        double  cant = 1;
+        int opcion_material=1,opcion_dije=1,opcion_tipo=2,opcion_tipomoneda=0;
+        double resultado = Metodos.calculo_Spinner(cant,opcion_material,opcion_dije,opcion_tipo,opcion_tipomoneda);
+        assertEquals(80,resultado,0 );
+    }
+    @Test
+    public void cuerda_ancla_plata_pesos(){
+        double  cant = 6;
+        int opcion_material=1,opcion_dije=1,opcion_tipo=1,opcion_tipomoneda=1;
+        double resultado = Metodos.calculo_Spinner(cant,opcion_material,opcion_dije,opcion_tipo,opcion_tipomoneda);
+        assertEquals(1728000,resultado,0 );
+    }
 
 }
